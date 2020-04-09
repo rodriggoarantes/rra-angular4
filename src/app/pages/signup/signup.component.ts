8562241047c34080b8aaaafdd875d3f58d96c2bd
rodriggoarantes <rodriggoarantes@gmail.com>
2020-04-09 19:37:38 -0300

    #sunshine ajustes de layout na home e inicio

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,43 +1,43 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
-import { UserService } from "@app/services/user.service";
-import { User } from "@app/models/User";
+import { UserService } from '@app/services/user.service';
+import { User } from '@app/models/User';
 
 @Component({
-  selector: "app-signup",
-  templateUrl: "./signup.component.html",
-  styleUrls: ["./signup.component.css"]
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
   constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {}
 
-  signup(event: any) {
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    const confirmPass = event.target.confirmPass.value;
+  signup(form: NgForm) {
+    const { name, email, password, confirmPass } = form.value;
 
-    if (!name || !email || !password || !confirmPass) {
-      alert("Campos obrigatorios devem ser informados");
+    if (!form.valid || !name || !email || !password || !confirmPass) {
+      alert('Campos obrigatorios devem ser informados');
+      return;
     }
 
     if (password !== confirmPass) {
-      alert("Senha e confirmaçãos são divergentes");
+      alert('Senha e confirmação são divergentes');
+      return;
     }
 
     this.userService.create(name, email, password, confirmPass).subscribe(
       (retorno: User) => {
         if (retorno && retorno._id) {
-          alert("Usuario criado com sucesso");
+          alert('Usuario criado com sucesso');
         } else {
-          alert("Usuario não cadastrado");
+          alert('Usuario não cadastrado');
         }
-        this.router.navigate(["/login"]);
+        this.router.navigate(['/login']);
       },
-      e => {
+      (e) => {
         alert(e.error);
       }
     );
