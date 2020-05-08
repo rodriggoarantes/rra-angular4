2110306aaef0488c261b5507e6fd563a3bb0ef5d
rodrigo <rodrigo.arantes@totvs.com.br>
2020-05-08 17:58:38 -0300

    #sunshine ajustes de layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  /**
+    from(rotinas)
+      .pipe(mergeMap((rotina: string) => this.permissaoRotinaService.getPermissoesNaRotina(rotina), 20))
+      .subscribe((res) => {
+        console.log(JSON.stringify(res));
+      });
 
+    forkJoin(arrayObservable).subscribe((res) => console.log(JSON.stringify(res)));
+   */
 }
