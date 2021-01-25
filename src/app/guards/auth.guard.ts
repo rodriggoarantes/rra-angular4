7be92e5e4fd779b9c1058d0876655fff1ca6d895
Rodrigo Arantes <rodriggoarantes@gmail.com>
2021-01-25 09:34:48 -0300

    migrando para angular 9

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
 
   canActivate(): boolean {
     if (!this.auth.isAuth()) {
-      this.router.navigate(['']);
+      this.router.navigate(['/login']);
       return false;
     }
     return true;
