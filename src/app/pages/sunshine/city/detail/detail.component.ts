25714fcd8adfdf8a1ec9245f7618508ea4910fdc
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2021-02-11 17:32:42 -0300

    apresentando temperaturas no detalhes de cidades

--- src/app/pages/sunshine/city/detail/detail.component.ts
+++ src/app/pages/sunshine/city/detail/detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { Observable, of, Subscription } from 'rxjs';
-import { switchMap, first, mergeMap } from 'rxjs/operators';
+import { switchMap, first, mergeMap, map } from 'rxjs/operators';
 
 import { UiService } from '@app/services/ui.service';
 
@@ -20,6 +20,7 @@ import { CityWeather } from '@app/models/CityWeather';
 export class DetailComponent implements OnInit {
   private selectedCityId: string;
   private themeSubs: Subscription;
+  private cityWeatherStoreSubs: Subscription;
 
   darkMode: boolean;
   cityIllustrationPath = '/./assets/city-background-default.svg';
@@ -32,8 +33,8 @@ export class DetailComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     public uiService: UiService,
-    private cityUserStore: CityUserStoreService,
-    private weatherStore: CityWeatherStoreService
+    private readonly cityUserStore: CityUserStoreService,
+    private readonly weatherStore: CityWeatherStoreService
   ) {}
 
   ngOnInit() {
@@ -43,14 +44,39 @@ export class DetailComponent implements OnInit {
 
     this.route.paramMap.pipe(switchMap((params) => of(String(params.get('cityId'))))).subscribe((cityId) => {
       this.selectedCityId = cityId;
-      this.city$ = this.cityUserStore.cities$.pipe(
-        mergeMap((data: Array<City>) => data.filter((city: City) => city._id === this.selectedCityId)),
-        first()
-      );
+      this.city$ = this._observableCity();
+      this.weather$ = this._observableWeather();
     });
   }
 
   ngOnDestroy() {
     this.themeSubs.unsubscribe();
+  }
+
+  backHome() {
+    this.router.navigate(['/sunshine']);
+  }
+
+  // ---
+
+  private _observableCity(): Observable<City> {
+    return this.cityUserStore.cities$.pipe(
+      mergeMap((data: Array<City>) => data.filter((city: City) => city._id === this.selectedCityId)),
+      first()
+    );
+  }
+
+  private _observableWeather(): Observable<CityWeather> {
+    return this.weatherStore.weathers$.pipe(
+      mergeMap((data: Array<CityWeather>) => data.filter((item: CityWeather) => item.city._id === this.selectedCityId)),
+      first(),
+      map((item) => {
+        const weather = <CityWeather>{
+          ...item,
+          temp: Math.ceil(item.temp),
+        };
+        return weather;
+      })
+    );
   }
 }
